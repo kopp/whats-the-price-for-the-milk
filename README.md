393ac59d6923639651385c334a885266e932910f
Michael Kopp <kopp.michael@yahoo.de>
2023-10-23 22:28:30 +0200

    add callmebot.com api to send updates instead of failing in the workflow

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 Monitor prices for a certain piece of milk.
 
-A pipeline checks the price and if it is below a certain value, the pipeline
-_fails_ hence github sends out an email alert.
+A pipeline checks the price daily.
+
+It can be configured to _fail_ if it is below a certain value, then github sends
+out an email alert.
+It can also be configured to send a WhatsApp notification using
+[callmebot.com](https://www.callmebot.com/blog/free-api-whatsapp-messages/)
+if the price is below a certain value.
--- src/whats_the_price_for_the_milk/main.py
+++ src/whats_the_price_for_the_milk/main.py
@@ -2,7 +2,10 @@
 Get the current price for a certain milk.
 """
 
-from typing import Optional
+import os
+import sys
+from urllib.parse import quote
+from typing import NamedTuple, Optional
 from bs4 import BeautifulSoup
 import requests
 import typed_argparse as tap
@@ -12,12 +15,54 @@ PRODUCT_URL = "https://nomi.shop/oatly-haferdrink-voll-6er-pack"
 EXIT_OK = 0
 EXIT_FAIL = 1
 
+CALLMEBOT_URL = "https://api.callmebot.com/whatsapp.php?phone={phone}&text={text}&apikey={key}"
+ENV_CALLMEBOT_NUMBER = "CALLMEBOT_NUMBER"
+ENV_CALLMEBOT_TOKEN = "CALLMEBOT_TOKEN"
 
-class _Arguments(tap.TypedArgs):
-    is_above: Optional[float] = tap.arg(
-        help=f"Return {EXIT_OK} if price is above this threshold or {EXIT_FAIL} if below or equal.",
-        default=None,
+
+# callmebot -------------------------------------------------------------------
+
+
+class CallMeBotConnection(NamedTuple):
+    number: str
+    token: str
+
+
+def _get_callmebot_data_from_env() -> CallMeBotConnection:
+    error_message = ""
+    if ENV_CALLMEBOT_NUMBER not in os.environ:
+        error_message += f" Number expected in {ENV_CALLMEBOT_NUMBER}."
+    if ENV_CALLMEBOT_TOKEN not in os.environ:
+        error_message += f" Token expected in {ENV_CALLMEBOT_NUMBER}"
+    if len(error_message) > 0:
+        raise ValueError("Error retrieving callmebot data from environment:" + error_message)
+    return CallMeBotConnection(
+        number=os.environ[ENV_CALLMEBOT_NUMBER],
+        token=os.environ[ENV_CALLMEBOT_TOKEN],
     )
+
+
+def _get_callmebot_url(text: str) -> str:
+    connection = _get_callmebot_data_from_env()
+    return CALLMEBOT_URL.format(
+        phone=connection.number,
+        key=connection.token,
+        text=quote(text),
+    )
+
+
+def _send_callmebot_message(text: str) -> bool:
+    url = _get_callmebot_url(text)
+    response = requests.get(url)
+    if not response.ok:
+        print(
+            "Error sending callmebot message '{text}' to '{url}': '{response}' (text '{response.text}')",
+            file=sys.stderr,
+        )
+    return response.ok
+
+
+# web scraping ----------------------------------------------------------------
 
 
 def _get_current_price() -> float:
@@ -32,14 +77,41 @@ def _get_current_price() -> float:
     return price
 
 
+# CLI -------------------------------------------------------------------------
+
+
+class _Arguments(tap.TypedArgs):
+    check_is_above: Optional[float] = tap.arg(
+        help=f"Return {EXIT_OK} if price is above this threshold or {EXIT_FAIL} if below or equal."
+        " Note, that the script may fail for other reasons even if the price is above.",
+        default=None,
+    )
+    message_if_below: Optional[float] = tap.arg(
+        help="Send a message if value is below this threshold."
+        " Uses callmebot.com and expects phone number under environment variable"
+        f" {ENV_CALLMEBOT_NUMBER} and API key under {ENV_CALLMEBOT_TOKEN}.",
+        default=None,
+    )
+
+
 def _run(args: _Arguments) -> None:
     price = _get_current_price()
     print(price)
-    if args.is_above is not None:
-        return_code = EXIT_OK if price > args.is_above else EXIT_FAIL
-        raise SystemExit(return_code)
+
+    execution_ok = True
+    if args.message_if_below is not None and price < args.message_if_below:
+        text = f"Current price *{price:.2f} \N{euro sign}* is below {args.message_if_below:.2f} \N{euro sign} at {PRODUCT_URL}."
+        is_sent = _send_callmebot_message(text)
+        execution_ok = is_sent and execution_ok
+    if args.check_is_above is not None:
+        is_above = price > args.check_is_above
+        execution_ok = execution_ok and is_above
+
+    return_code = EXIT_OK if execution_ok else EXIT_FAIL
+    raise SystemExit(return_code)
 
 
 def main():
     """Entry point of CLI script."""
-    tap.Parser(_Arguments, description=__doc__).bind(_run).run()
+    description = __doc__ + f" Checking website {PRODUCT_URL} for that."
+    tap.Parser(_Arguments, description=description).bind(_run).run()
